092e2b748ad1b790efab73777bee20fa68dba6ea
21cm0111 <92592180+3Natsumi9@users.noreply.github.com>
2022-12-07 11:37:38 +0900

    first commit

---
+++ AutoClip/AutoClipApp.swift
@@ -0,0 +1,17 @@
+//
+//  AutoClipApp.swift
+//  AutoClip
+//
+//  Created by cmStudent on 2022/12/07.
+//
+
+import SwiftUI
+
+@main
+struct AutoClipApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AutoClip/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  AutoClip
+//
+//  Created by cmStudent on 2022/12/07.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
